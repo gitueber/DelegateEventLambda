4289b788570d75186d86c40c75a591d1393513ba
gitueber <g1t.ueber@gmail.com>
2023-06-04 17:06:59 +0200

    Generic Delegate

---
+++ 03-GenericDelegate/Program.cs
@@ -0,0 +1,17 @@
+﻿MyGenericDelegate<string> strTarget = new(StringTarget);
+strTarget("Some string data");
+
+MyGenericDelegate<int> intTarget = new(IntTarget);
+intTarget(42);
+
+static void StringTarget(string arg)
+{
+    Console.WriteLine($"arg in uppercase is: {arg.ToUpper()}");
+}
+
+static void IntTarget(int arg)
+{
+    Console.WriteLine($"++arg is: {++arg}");
+}
+
+public delegate void MyGenericDelegate<T>(T arg);
