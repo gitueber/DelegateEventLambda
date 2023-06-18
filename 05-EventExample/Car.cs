feade526cfd16ddeb6620c9772671a3c899d5e7c
gitueber <g1t.ueber@gmail.com>
2023-06-18 07:34:24 +0200

    Simple Event example

---
+++ 05-EventExample/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_EventExample;
+
+public class Car
+{
+    private bool _carIsDead;
+
+    public int CurrentSpeed { get; set; }
+    public int MaxSpeed { get; set; } = 100;
+    public string PetName { get; set; }
+
+    public delegate void CarEngineHandler(string msg);
+
+    public Car(string petName, int maxSpeed, int currentSpeed)
+    {
+        PetName = petName;
+        MaxSpeed = maxSpeed;
+        CurrentSpeed = currentSpeed;
+    }
+
+    public event CarEngineHandler? Exploded;
+    public event CarEngineHandler? AboutToBlow;
+
+    public void Accelerate(int delta)
+    {
+        if (_carIsDead)
+        {
+            Exploded?.Invoke("Sorry, this car is dead");
+        }
+        else
+        {
+            CurrentSpeed += delta;
+
+            if (10 == MaxSpeed - CurrentSpeed)
+            {
+                AboutToBlow?.Invoke("Careful buddy! Gonna blow!");
+            }
+
+            if (CurrentSpeed >= MaxSpeed)
+            {
+                _carIsDead = true;
+            }
+            else
+            {
+                Console.WriteLine($"CurrentSpeed = {CurrentSpeed}");
+            }
+        }
+    }
+}
---
+++ 05-EventExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using _05_EventExample;
+
+Car car = new("SlubBug", 100, 10);
+// registering to events
+car.AboutToBlow += CarIsAlmostDoomed;
+car.AboutToBlow += CarAboutToBlow;
+
+// here just an example, using the method group conversion syntax with events
+Car.CarEngineHandler engineHandler = CarExploded;
+car.Exploded += engineHandler;
+
+Console.WriteLine("#### Speeding up ####");
+for (int i = 0; i < 6; i++)
+{
+    car.Accelerate(20);
+}
+
+// unregister CarExploded method -> message 'Sorry, this car is dead' won't be printed for each iteration
+car.Exploded -= engineHandler;
+Console.WriteLine("#### Speeding up ####");
+for (int i = 0; i < 6; i++)
+{
+    car.Accelerate(20);
+}
+
+
+static void CarAboutToBlow(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+static void CarIsAlmostDoomed(string msg)
+{
+    Console.WriteLine($"=> Critical Message from Car: {msg}");
+}
+
+static void CarExploded(string msg)
+{
+    Console.WriteLine(msg);
+}
